6e9892ed61adb826765caa9a0cdb10da623aa542
Rama Faisal <ramafaisal23@gmail.com>
2024-12-26 21:30:34 +0700

    task8

--- hasilAkhirReact/src/App.jsx
+++ hasilAkhirReact/src/App.jsx
@@ -1,15 +1,11 @@
-import React from "react";
-import AdminLayout from "./layouts/AdminLayout";
-import Mahasiswa from "./pages/Admin/Mahasiswa";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import RouteList from "./RouteList.jsx"
 
-function App() {
-  return (
-    <>
-      <AdminLayout>
-        <Mahasiswa />
-      </AdminLayout>
-    </>
-  );
-}
-
-export default App;
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={RouteList} />
+  </StrictMode>
+);
--- hasilAkhirReact/src/layouts/AdminLayout.jsx
+++ hasilAkhirReact/src/layouts/AdminLayout.jsx
@@ -1,6 +1,7 @@
 import React, { Children } from "react";
+import { Outlet, Link } from "react-router-dom";
 
-const AdminLayout = ({ children }) => {
+const AdminLayout = () => {
   return (
     <>
       <div className="flex h-screen bg-gray-100">
@@ -9,24 +10,30 @@ const AdminLayout = ({ children }) => {
           <nav>
             <ul className="text-white p-4">
               <li className="p-2 hover:bg-white hover:text-black rounded">
-                <a href="">Dashboard</a>
+                <Link to="/admin">Dashboard</Link>
+                {/* <link to="/admin">Dashboard</link> */}
               </li>
               <li className="p-2 hover:bg-white hover:text-black rounded">
-                <a href="">Mahasiswa</a>
+                <Link to={'/admin/mahasiswa'}>Mahasiswa</Link>
               </li>
               <li className="p-2 hover:bg-white hover:text-black rounded">
-                <a href="">Settings</a>
+                <Link>Settings</Link>
               </li>
             </ul>
           </nav>
         </aside>
+
         <div className="flex-1 flex flex-col">
           <header className="bg-white shadow p-4 text-right">
             <button className="bg-blue-500 hover:bg-blue-700 p-2 rounded-md text-white">
               Log out
             </button>
           </header>
-          <main className="flex-grow p-6 bg-blue-50">{ children }</main>
+
+          <main className="flex-grow p-6 bg-blue-50">
+            <Outlet />
+          </main>
+
           <footer className="bg-indigo-900 p-4 text-white text-center">
             &copy; Admin Panel
           </footer>
